f8557f0c68f3d44819400b583052897e12c63d0d
Bruno Gomes <brunog.damascena@gmail.com>
2024-12-05 23:15:01 -0300

    tudo pronto para iniciar as validações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,18 @@ import {CommonModule} from "@angular/common";
 import {DropdownModule} from "primeng/dropdown";
 import {ButtonModule} from "primeng/button";
 import {DialogModule} from "primeng/dialog";
+import {InputTextModule} from "primeng/inputtext";
 
 import {TemplateService} from "./service/template.service";
 
-import {TemplateMessage} from "./model/templateMessage";
+import {TemplateMessage} from "./model/template-message/templateMessage";
+import {Department} from "./model/department";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, DropdownModule, DialogModule, ButtonModule],
+  imports: [CommonModule, DropdownModule, DialogModule, ButtonModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -23,6 +25,28 @@ export class AppComponent {
   isModal: boolean = false;
   loadingButton: boolean = false;
   disabledButton: boolean = false;
+  departments: Department[] = [
+    {
+      "id": 1,
+      "title": "Suporte"
+    },
+    {
+      "id": 2,
+      "title": "Comercial"
+    },
+    {
+      "id": 3,
+      "title": "Financeiro"
+    },
+    {
+      "id": 4,
+      "title": "Desenvolvimento"
+    },
+    {
+      "id": 5,
+      "title": "Implantação"
+    }
+  ];
 
   constructor(private readonly templateService: TemplateService) {
     this.templateMessage = templateService.getTemplateMessage();
@@ -36,5 +60,10 @@ export class AppComponent {
       this.isModal = true;
       this.disabledButton = true;
     }, 1000);
+  }
+
+  closeModal() {
+    this.isModal = false;
+    this.disabledButton = false;
   }
 }
--- src/app/service/template.service.ts
+++ src/app/service/template.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {TemplateMessage} from "../model/templateMessage";
+import {TemplateMessage} from "../model/template-message/templateMessage";
 
 @Injectable({
   providedIn: 'root'
